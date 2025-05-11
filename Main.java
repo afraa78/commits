ef1a58693ff0331a65dc31d3bb59c22ba1ad1851
afraa78 <afraamoh178@gmail.com>
2025-05-11 22:04:54 +0300

    edit

--- Main.java
+++ Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ids 20230709 20230721 20230750");
     }
+    public static void man(String[] args) {
+        System.out.println("Ids 20230709 20230721 20230750");
+    }
 }
